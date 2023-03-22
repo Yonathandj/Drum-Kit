26c9f261fdb40bfdd66f26d7efce69522e4d5492
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-03-22 16:22:46 +0700

    Finishing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll("button");
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const buttonSound = new Audio(`sounds/${button.classList[1]}.mp3`);
