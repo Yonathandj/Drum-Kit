29df83f18e41f0ca3751dc30cb86ab4d1bc08336
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-03-22 16:09:18 +0700

    Add functionality when user click predefined key

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ buttons.forEach((button) => {
     }, 20);
   });
 });
+
+buttons.forEach((button) => {
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+      const buttonSound = new Audio(`sounds/kick-bass.mp3`);
+      buttonSound.play();
+      setTimeout(() => {
+        buttonSound.currentTime = 0;
+      }, 20);
+    } else if (e.key.toUpperCase() === button.innerHTML) {
+      const buttonSound = new Audio(`sounds/${button.classList[1]}.mp3`);
+      buttonSound.play();
+      setTimeout(() => {
+        buttonSound.currentTime = 0;
+      }, 20);
+    } else {
+      return;
+    }
+  });
+});
